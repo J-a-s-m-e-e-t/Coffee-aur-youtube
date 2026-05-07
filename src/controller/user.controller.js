4537174df2a90d0483819a4c0b4dfa305484b438
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-05-07 11:34:13 +0530

    typo

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -320,7 +320,7 @@ const getWatchHistory=asyncHandler(async(req,res)=>{
     const user=await  User.aggregate([
         {
             $match:{
-                id:new mongoose.Types.ObjectId(req.user._id)
+                _id:new mongoose.Types.ObjectId(req.user._id)
             }
         },
         {
